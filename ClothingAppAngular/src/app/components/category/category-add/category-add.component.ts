c55120b45fbbb0b61b5d2c999f46aa234a16e3ea
00011270 <wiut11270@gmail.com>
2023-04-07 12:39:46 +0500

    Added hero section and category adding and category dropdown gets shows names from DB

---
+++ ClothingAppAngular/src/app/components/category/category-add/category-add.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IProductCategory, ProductCategory } from 'src/app/models/productCategory.model';
+import { ProductCategoryService } from 'src/app/services/product-category.service';
+import { NavBlockComponent } from '../../navigation/nav-block/nav-block.component';
+
+@Component({
+  selector: 'app-category-add',
+  templateUrl: './category-add.component.html',
+  styleUrls: ['./category-add.component.css']
+})
+export class CategoryAddComponent {
+  category: IProductCategory = new ProductCategory()
+
+  constructor(private productCategoryService: ProductCategoryService,
+              private router: Router){
+
+  }
+
+  addCategory(){
+    this.productCategoryService.addCategory(this.category).subscribe({
+      next:(category) =>{
+        this.category = category;
+        this.router.navigate([''])
+        console.log(category);
+        
+      },
+      error:(Response)=>{
+        alert('An error occurred while creating new category. Please check the fields before submitting.');
+        console.log(Response);
+      }
+    })
+  }
+}
